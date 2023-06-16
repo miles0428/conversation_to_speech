8125c9549892d84eadb39242a35820835ce4eb80
YC-Chung <tearofclown0428@gmail.com>
2023-06-16 23:39:05 +0800

    rename main.py to one_sentence.py

--- one_sentence.py
+++ one_sentence.py
@@ -3,6 +3,7 @@ from google.oauth2 import service_account
 import os
 from pydub import AudioSegment
 
+
 def text_to_speech(text : str, file_name : str, key :str = 'key.json' ) -> None:
     '''
     convert a chinese text to speech and save it to file_name in mp3 format
@@ -19,38 +20,33 @@ def text_to_speech(text : str, file_name : str, key :str = 'key.json' ) -> None:
     credentials = service_account.Credentials.from_service_account_file(key)
 
     client = tts.TextToSpeechClient(credentials=credentials)
-    # Set the text input to be synthesized
+
     synthesis_input = tts.SynthesisInput(text=text)
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
     voice = tts.VoiceSelectionParams(
         language_code="cmn-TW",
         name='cmn-TW-Standard-A'
     )
 
-    # Select the type of audio file you want returned
     audio_config = tts.AudioConfig(
         audio_encoding=tts.AudioEncoding.MP3
     )
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
     response = client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
-    # The response's audio_content is binary.
     with open("temp.mp3", "wb") as out:
-        # Write the response to the output file.
         out.write(response.audio_content)
 
     #add a 1 sec silence to the end of the file
     temp = AudioSegment.from_mp3("temp.mp3")
     temp = temp + AudioSegment.silent(duration=1000)
     temp.export(file_name, format="mp3")
+
     #rm temp file
     os.remove("temp.mp3")
+
 
 def get_duration(file_name : str) -> int:
     '''
@@ -65,6 +61,7 @@ def get_duration(file_name : str) -> int:
     temp = AudioSegment.from_mp3(file_name)
     duration = len(temp)
     return duration
+
 
 def generate_srt_style_text(
         text : str,
